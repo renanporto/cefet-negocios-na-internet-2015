9b0532b3dcd342822e863d6d15a9f378594ebf56
Renan Porto <renanporto92@yahoo.com.br>
2015-03-25 16:09:06 -0300

    Criando a home do site

--- FantasyStore.WebApp/Controllers/HomeController.cs
+++ FantasyStore.WebApp/Controllers/HomeController.cs
@@ -13,24 +13,24 @@ namespace FantasyStore.WebApp.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            var products = unitOfWork.Products.GetAll();
-            return View(products);
+            return View();
         }
 
-        public ActionResult Index(string term)
-        {
-            var productsByCategory = unitOfWork.Products.GetByCategory(term);
-            if (!productsByCategory.Any())
-            {
-                var productsByBrand = unitOfWork.Products.GetByBrand(term);
-                if (!productsByBrand.Any())
-                {
-                    //Retornar uma página amigável com esse conteúdo
-                    return Content(string.Format("Não há produtos na categoria ou marca {0}", term));
-                }
-                return View(productsByBrand);
-            }
-            return View(productsByCategory);
-        }
+
+        //public ActionResult Index(string term)
+        //{
+        //    var productsByCategory = unitOfWork.Products.GetByCategory(term);
+        //    if (!productsByCategory.Any())
+        //    {
+        //        var productsByBrand = unitOfWork.Products.GetByBrand(term);
+        //        if (!productsByBrand.Any())
+        //        {
+        //            //Retornar uma página amigável com esse conteúdo
+        //            return Content(string.Format("Não há produtos na categoria ou marca {0}", term));
+        //        }
+        //        return View(productsByBrand);
+        //    }
+        //    return View(productsByCategory);
+        //}
     }
 }
