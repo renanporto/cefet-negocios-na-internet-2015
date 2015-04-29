c5c28553d7b2a92b35c9ac4d6e86e21e6f801c3b
Renan Porto <renanporto92@yahoo.com.br>
2015-04-28 22:37:09 -0300

    Criando viewmodel de login

--- FantasyStore.WebApp/Controllers/AuthController.cs
+++ FantasyStore.WebApp/Controllers/AuthController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using FantasyStore.WebApp.ViewModels;
 
 namespace FantasyStore.WebApp.Controllers
 {
@@ -11,9 +13,64 @@ namespace FantasyStore.WebApp.Controllers
     {
         //
         // GET: /Auth/
-        public ActionResult LogIn()
+        public ActionResult LogIn(string returnUrl)
         {
+            var model = new LogInModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LogInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // Don't do this in production!
+            if (model.Email == "admin@admin.com" && model.Password == "password")
+            {
+                var identity = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, "Renan"),
+                new Claim(ClaimTypes.Email, "renan.porto@vtex.com.br"),
+                new Claim(ClaimTypes.Country, "Brazil")
+            },
+                    "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+                
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // user authN failed
+            ModelState.AddModelError("", "Invalid email or password");
             return View();
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("index", "home");
         }
 	}
 }
